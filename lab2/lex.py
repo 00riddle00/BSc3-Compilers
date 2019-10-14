8b85fd5d119a9febdd44fc4ea866e92f16428a28
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-10-14 06:56:44 +0300

    .

--- lab2/lex.py
+++ lab2/lex.py
@@ -2,7 +2,7 @@ from sys import argv
 
 from lexer import Lexer
 
-file_to_lex = 'sample2.tm'
+file_to_lex = 'sample3.tm'
 if len(argv) == 2:
     file_to_lex = argv[1]
 
--- lab2/lexer/lexer.py
+++ lab2/lexer/lexer.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 
 KEYWORDS = {
   'if': ':KW_IF',
@@ -27,7 +26,6 @@ class Lexer:
     state: str
     tokens: list
     token_start: int
-    token_start_ln: int
     running: bool
     curr_char: str
 
@@ -39,7 +37,6 @@ class Lexer:
         self.state = ':START'
         self.tokens = []
         self.token_start = 0
-        self.token_start_ln = 1
         self.running = True
         self.curr_char = ''
 
@@ -47,22 +44,35 @@ class Lexer:
         self.buffer += self.curr_char
 
     def begin_token(self, new_state):
-        self.token_start_ln = self.line_no
+        self.token_start = self.line_no
         self.state = new_state
 
-    def complete_token(self, token_type):
-        self.tokens.append(Token(token_type, self.buffer, self.token_start_ln))
+    def complete_ident(self):
+        if self.buffer in KEYWORDS:
+            kw_type = KEYWORDS[self.buffer]
+            self.buffer = ''
+            self.complete_token(kw_type, False)
+        else:
+            self.complete_token(':IDENT', False)
+
+    def complete_token(self, token_type, advance=True):
+        self.tokens.append(Token(token_type, self.buffer, self.token_start))
         # print(f'token: {token_type} {self.buffer}')
         self.buffer = ''
         self.state = ':START'
+        if not advance:
+            self.offset -= 1
 
     def dump_tokens(self):
-        print(f'{"ID":>2}| {"LN":>2}| {"TYPE":<12} | {"VALUE"}')
+        print(f'{"ID":>3}| {"LN":>3}| {"TYPE":<10} | {"VALUE":<10}')
         for index, token in enumerate(self.tokens):
-            print(f'{index:>2}| {token.line_no:>2}| {token.type:<12} | {token.value}')
+            print(f'{index:>3}| {token.line_no:>3}| {token.type:<10} | {token.value:<10}')
 
-    def error(self):
-        self.lexer_error('unexpected symbol', self.curr_char)
+    def error(self, msg=None):
+        if not msg:
+            msg = f'unexpected input character {self.curr_char}'
+
+        print(f'sample.fx:{self.line_no} lexer error: {msg}')
         self.running = False
 
     def lex_all(self):
@@ -73,15 +83,20 @@ class Lexer:
             self.lex_char()
             self.offset += 1
 
-        if self.running:
-            self.curr_char = '\n'
-            self.lex_char()
-            if self.state != ':START':
-                self.lexer_error(f'unterminated something {self.state}', None)
+        self.curr_char = ' '
+        self.lex_char()
+
+
+        if self.state == ':START':
+            self.complete_token(':EOF')
+        elif self.state == ':LIT_STR':
+            self.error('unterminated string')
+        else:
+            self.error(f'unterminated token: {self.state}')
 
     def lex_char(self):
-        if self.state == ':COMMENT':
-            self.lex_comment()
+        if self.state == ':COMMENT_SL':
+            self.lex_comment_sl()
         elif self.state == ':IDENT':
             self.lex_ident()
         elif self.state == ':LIT_INT':
@@ -95,9 +110,9 @@ class Lexer:
         elif self.state == ':START':
             self.lex_start()
         else:
-            raise Exception(f'invalid state')
+            raise Exception(f'bad state {self.state}')
 
-    def lex_comment(self):
+    def lex_comment_sl(self):
         if self.curr_char == '\n':
             self.line_no += 1
             self.state = ':START'
@@ -108,63 +123,73 @@ class Lexer:
         if self.curr_char in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
                               'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
             self.add()
-            return
-
-        self.rewind()
-
-        if self.buffer in KEYWORDS:
-            kw_type = KEYWORDS[self.buffer]
-            self.buffer = ''
-            self.complete_token(kw_type)
+        elif self.curr_char in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                                'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
+            self.add()
+        elif self.curr_char in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            self.add()
+        elif self.curr_char == '_':
+            self.add()
         else:
-            self.complete_token(':IDENT')
+            self.complete_ident()
 
     def lex_lit_int(self):
         if self.curr_char in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
             self.add()
         else:
-            self.rewind()
-            self.complete_token(':LIT_INT')
+            self.complete_token(':LIT_INT', False)
 
     def lex_lit_str(self):
-        if self.curr_char == '\n':
-            self.add()
-            self.line_no += 1
+        if self.curr_char == '"':
+            self.complete_token(':LIT_STR')
         elif self.curr_char == '\\':
             self.state = ':LIT_STR_ESCAPE'
-        elif self.curr_char == '"':
-            self.complete_token(':LIT_STR')
+        elif self.curr_char == '\n':
+            self.add()
+            self.line_no += 1
         else:
             self.add()
 
     def lex_lit_str_escape(self):
-        if self.curr_char == 'n':
-            self.buffer += '\n'
-        elif self.curr_char == 't':
-            self.buffer += '\t'
         if self.curr_char == '"':
-            self.buffer += '\"'
+            self.buffer += "\""
+        elif self.curr_char == 't':
+            self.buffer += "\t"
+        elif self.curr_char == 'n':
+            self.buffer += "\n"
         else:
-            self.lexer_error('invalid_escape symbol', self.curr_char)
+            self.error(f'invalid escape sequence "\\{self.curr_char}"')
         self.state = ':LIT_STR'
 
     def lex_op_l(self):
         if self.curr_char == '=':
             self.complete_token(':OP_LE')
         else:
-            self.rewind()
-            self.complete_token(':OP_L')
+            self.complete_token(':OP_L', False)
 
     def lex_start(self):
-        if self.curr_char == '#':
-            self.state = ':COMMENT'
-        elif self.curr_char in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+        if self.curr_char in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                              'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+            self.add()
+            self.begin_token(':IDENT')
+        elif self.curr_char in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                                'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
+            self.add()
+            self.begin_token(':IDENT')
+        elif self.curr_char == '_':
             self.add()
             self.begin_token(':IDENT')
         elif self.curr_char in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
             self.add()
-            self.begin_token(':LIT_INT')
+            self.begin_token(':LIT_INT')  # FIX
+        elif self.curr_char == '"':
+            self.begin_token(':LIT_STR')
+        elif self.curr_char == '#':
+            self.state = ':COMMENT_SL'
+        elif self.curr_char == ' ':
+            pass  # ignore
+        elif self.curr_char == '\n':
+            self.line_no += 1
         elif self.curr_char == '+':
             self.begin_token(':START')
             self.complete_token(':OP_PLUS')
@@ -173,18 +198,5 @@ class Lexer:
         elif self.curr_char == '=':
             self.begin_token(':START')
             self.complete_token(':OP_E')
-        elif self.curr_char == '"':
-            self.begin_token(':LIT_STR')
-        elif self.curr_char == ' ':
-            pass  # ignore
-        elif self.curr_char == '\n':
-            self.line_no += 1
         else:
             self.error()
-
-    def lexer_error(self, msg, var_to_pprint):
-        print(f'[program_name.fx]:{self.line_no}: lexer error: {msg}'),
-        pprint(var_to_pprint)
-
-    def rewind(self):
-        self.offset -= 1
